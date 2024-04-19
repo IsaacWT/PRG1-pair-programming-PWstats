f49e96a285282d6e7854178fd9fd10ce35cc9870
IsaacWT <167076966+IsaacWT@users.noreply.github.com>
2024-04-19 14:27:32 +0000

    counter exporting to csv file

--- stats.js
+++ stats.js
@@ -11,9 +11,16 @@ if (fs.existsSync(outputFile)) { // check for existing output file
 const data = fs.readFileSync(inputFile, "utf-8");
 const lines = data.split(/\n/);
 
+const passwordLengthTotal = new Array(20).fill(0);
+const arr = Array.from({length: 20}, (e, i)=> i)
+
 for (line of lines) {
       
-      elements = line.split(delimiter);
-      console.log(elements)
-}
-
+  const elements = line.split(delimiter);
+  const passwordLength = elements[1].length;
+  passwordLengthTotal[passwordLength]++;
+} 
+      
+for (i = 0; i < passwordLengthTotal.length; i++){
+fs.appendFileSync(outputFile, `Chars: ${arr[i]}, Count: ${passwordLengthTotal[i]} \n`, "utf-8");
+}      
